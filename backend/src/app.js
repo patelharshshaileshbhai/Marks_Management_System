aeb09f7f73a01813848d568bb349c9682003ea2d
Harsh <ph335579@gmail.com>
2024-09-29 10:49:30 +0530

    changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,15 +21,15 @@ const __dirname = path.dirname(__filename);
 
 
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 
-app.use(cors({
-    CORS_ORIGIN:"http://localhost:5173",
-    // origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+const corsOptions = {
+    origin: ["https://marks-management-system.vercel.app/","http://localhost:5173"],
+    credentials: true,
+  };
+  app.use(cors(corsOptions));
 
 
 
